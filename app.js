0d178c4215673cb0c64eb85abeda7b387adc5f61
godbs129 <junhaeyoon@gmail.com>
2020-11-24 08:28:59 +0900

    cors

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
 const api = require('./api');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 app.use(api);
 
